2554d458f4eddb37e229e62d544912048c812a86
Pallab Chandra Das <pallab15-5698@diu.edu.bd>
2026-09-14 21:02:51 +0600

    88. Merge Sorted Array solved without using extra spaces

--- two_pointer_approach/leetcode_88.cpp
+++ two_pointer_approach/leetcode_88.cpp
@@ -4,6 +4,40 @@
 using namespace std;
 ///88. Merge Sorted Array
 
+
+  /// without using extra spaces
+    // void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+
+    //     int size = nums1.size() - 1;
+    //         m = m-1; n = n-1;
+    //         while(m >= 0  && n >= 0)
+    //         {
+    //             if(nums1[m] < nums2[n])
+    //             {
+    //                 nums1[size--] = nums2[n--];
+                    
+    //             }
+    //             else{
+    //                 nums1[size--] = nums1[m--];
+    //             }
+    //         }
+    //         // check for if left alement inside first vector
+    //         // put inside the num1
+    //         while(m >= 0)
+    //         {
+    //             nums1[size--] = nums1[m--];
+    //         }
+    //         // check for if left alement inside second vector
+    //         // put inside the num1
+    //         while(n >= 0)
+    //         {
+    //             nums1[size--] = nums2[n--];
+    //         }
+            
+    //     }
+
+
+    /// usinfg extra space temp vecotr<int>
  void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
       long long int start = 0 , first = 0 , second = 0 ;
         vector<int> temp = nums1 ;
